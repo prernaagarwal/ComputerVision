fd544acec740f382b988e22082b66894dcee5426
prernaagarwal <prerna214@gmail.com>
2019-01-24 11:15:24 -0800

    using loops for matrix multiplication

--- hw1/color_transfer2.py
+++ hw1/color_transfer2.py
@@ -32,19 +32,25 @@ def convert_color_space_RGB_to_Lab(img_RGB):
     convert image color space RGB to Lab
     '''
     img_LMS = np.zeros_like(img_RGB,dtype=np.float32)
-   
-    LMStoRGB = np.array([[0.3811,0.5783,0.0402],[0.1967,0.7244,0.0782],[0.0241,0.1288,0.8444]])
-    img_LMS = np.matmul(img_RGB,LMStoRGB)
-    #print(img_LMS)
-
-    img_LMS = np.log10(img_LMS)
-
     img_Lab = np.zeros_like(img_RGB,dtype=np.float32)
-    matrix1 = [[1/math.sqrt(3),0,0],[0,1/math.sqrt(6),0],[0,0,1/math.sqrt(2)]]
-    matrix2 = [[1,1,1],[1,1,-2],[1,-1,0]]
-    matrix3 = np.matmul(matrix1,matrix2)
-    img_Lab = np.matmul(img_LMS,matrix3)
-     
+   
+    
+    constm1 = np.array([[0.3811,0.5783,0.0402],[0.1967,0.7244,0.0782],[0.0241,0.1288,0.8444]])
+    constm2 = [[1/math.sqrt(3),0,0],[0,1/math.sqrt(6),0],[0,0,1/math.sqrt(2)]]
+    constm3 = [[1,1,1],[1,1,-2],[1,-1,0]]
+    constm4 = np.matmul(constm2,constm3)
+
+    row,col,depth = img_RGB.shape
+    for i in range(row):
+        for j in range(col):
+            RGBm = img_RGB[i,j]
+            LMSm = np.matmul(constm1, RGBm)
+            LMSlog = np.log10(LMSm)
+
+            LAB = np.matmul(constm4,LMSlog)
+            img_Lab[i,j] = LAB
+
+    #print(img_Lab)
     return img_Lab
 
 def convert_color_space_Lab_to_RGB(img_Lab):
@@ -52,22 +58,22 @@ def convert_color_space_Lab_to_RGB(img_Lab):
     convert image color space Lab to RGB
     '''
     img_LMS = np.zeros_like(img_Lab,dtype=np.float32)
-
-    matrix1 = [[1,1,1],[1,1,-1],[1,-2,0]]
-    matrix2 = [[math.sqrt(3)/3.0,0,0],[0,math.sqrt(6)/6.0,0],[0,0,math.sqrt(2)/2.0]]
-    matrix3 = np.matmul(matrix1,matrix2)
-    img_LMS = np.matmul(img_Lab, matrix3) 
-    #img_LMS = np.matmul(matrix3, img_Lab)
-    
-    #take power here of Lms so 10 to the power LMS
-    img_LMS = np.power(10,img_LMS)
-    
-
     img_RGB = np.zeros_like(img_Lab,dtype=np.float32)
 
-    matrix4 = [[4.4679, -3.5873, 0.1193],[-1.2186,2.3809,-0.1624],[0.0497, -0.2439, 1.2045]]
-    img_RGB = np.matmul(img_LMS, matrix4)
+    constm1 = [[1,1,1],[1,1,-1],[1,-2,0]]
+    constm2 = [[math.sqrt(3)/3.0,0,0],[0,math.sqrt(6)/6.0,0],[0,0,math.sqrt(2)/2.0]]
+    constm3 = np.matmul(constm1,constm2)
+    constm4 = [[4.4679, -3.5873, 0.1193],[-1.2186,2.3809,-0.1624],[0.0497, -0.2439, 1.2045]]
+    
+    row,col,depth = img_RGB.shape
+    for i in range(row):
+        for j in range(col):
+            Labm  = img_Lab[i,j]
+            LMSm = np.matmul(constm3, Labm)
+            LMS10 = np.power(10,LMSm)
 
+            RGB = np.matmul(constm4,LMS10)
+            img_RGB[i,j] = RGB
 
     return img_RGB
 
@@ -76,7 +82,19 @@ def convert_color_space_RGB_to_CIECAM97s(img_RGB):
     convert image color space RGB to CIECAM97s
     '''
     img_CIECAM97s = np.zeros_like(img_RGB,dtype=np.float32)
-    # to be completed ...
+
+    constm1 = np.array([[0.3811,0.5783,0.0402],[0.1967,0.7244,0.0782],[0.0241,0.1288,0.8444]])
+    constm2 = [[2.00,1.00,0.05],[1.00,-1.09,0.09],[0.11,0.11,-0.22]]
+
+    row,col,depth = img_RGB.shape
+    for i in range(row):
+        for j in range(col):
+            RGBm = img_RGB[i,j]
+            LMSm = np.matmul(constm1, RGBm)
+
+            CAM = np.matmul(constm2,LMSm)
+            img_CIECAM97s[i,j] = CAM
+
 
     return img_CIECAM97s
 
@@ -85,7 +103,21 @@ def convert_color_space_CIECAM97s_to_RGB(img_CIECAM97s):
     convert image color space CIECAM97s to RGB
     '''
     img_RGB = np.zeros_like(img_CIECAM97s,dtype=np.float32)
-    # to be completed ...
+
+
+    constm1 = [[1,1,1],[1,1,-1],[1,-2,0]]
+    constm2 = [[math.sqrt(3)/3.0,0,0],[0,math.sqrt(6)/6.0,0],[0,0,math.sqrt(2)/2.0]]
+    constm3 = np.matmul(constm1,constm2)
+    constm4 = [[4.4679, -3.5873, 0.1193],[-1.2186,2.3809,-0.1624],[0.0497, -0.2439, 1.2045]]
+    
+    row,col,depth = img_CIECAM97s.shape
+    for i in range(row):
+        for j in range(col):
+            CAMm  = img_CIECAM97s[i,j]
+            LMSm = np.matmul(constm3, CAMm)
+
+            RGB = np.matmul(constm4,LMSm)
+            img_RGB[i,j] = RGB
 
     return img_RGB
 
@@ -98,14 +130,13 @@ def color_transfer_in_Lab(img_RGB_source, img_RGB_target):
     
     new_src_img_Lab = convert_color_space_RGB_to_Lab(new_rgb_img)
     new_tar_img_Lab = convert_color_space_RGB_to_Lab(target_rgb) 
-
-
+    
     combined_LAB = np.zeros_like(new_src_img_Lab,dtype=np.float32)
 
     l = new_src_img_Lab[:,:,0]
     a = new_src_img_Lab[:,:,1]
     b = new_src_img_Lab[:,:,2]
-    print(l.shape)    
+
     l_tar = new_tar_img_Lab[:,:,0]
     a_tar = new_tar_img_Lab[:,:,1]
     b_tar = new_tar_img_Lab[:,:,2]
@@ -113,7 +144,6 @@ def color_transfer_in_Lab(img_RGB_source, img_RGB_target):
     lmean = np.mean(l)
     amean = np.mean(a)
     bmean = np.mean(b)
-    print(lmean.shape)
 
     tar_lmean = np.mean(l_tar)
     tar_amean = np.mean(a_tar)
@@ -128,29 +158,17 @@ def color_transfer_in_Lab(img_RGB_source, img_RGB_target):
     tar_b_std = np.std(b_tar)
     
 
-###########################################
     lstar_src = l - lmean 
     astar_src = a - amean 
     bstar_src = b - bmean 
-
-    #labstar = new_src_img_Lab - np.mean(new_src_img_Lab,axis=(0,1))
-    #src_std =  np.std(new_src_img_Lab, axis=(0,1))
-    #target_std =  np.std(new_tar_img_Lab, axis=(0,1))
 
     ldash1 = (tar_l_std/src_l_std) * lstar_src
     adash1 = (tar_a_std/src_a_std) * astar_src
     bdash1 = (tar_b_std/src_b_std) * bstar_src
 
-    #labdash = target_std/src_std *labstar
-    #mean_target = np.mean(new_tar_img_Lab, axis = (0,1))
-    
-    
     ldash_new = ldash1 + tar_lmean
     adash_new = adash1 + tar_amean
     bdash_new = bdash1 + tar_bmean
-
-
-##########################################################
 
     combined_LAB[:,:,0] = ldash_new
     combined_LAB[:,:,1] = adash_new
@@ -169,7 +187,63 @@ def color_transfer_in_RGB(img_RGB_source, img_RGB_target):
 
 def color_transfer_in_CIECAM97s(img_RGB_source, img_RGB_target):
     print('===== color_transfer_in_CIECAM97s =====')
-    # to be completed ...
+    
+    new_rgb_img = convert_color_space_BGR_to_RGB(img_RGB_source)
+    target_rgb = convert_color_space_BGR_to_RGB(img_RGB_target)
+
+    img_src = convert_color_space_RGB_to_CIECAM97s(new_rgb_img)
+    img_tar = convert_color_space_RGB_to_CIECAM97s(target_rgb)
+
+
+
+    combined_CAM = np.zeros_like(img_src,dtype=np.float32)
+
+    l = img_src[:,:,0]
+    a = img_src[:,:,1]
+    b = img_src[:,:,2]
+
+    l_tar = img_tar[:,:,0]
+    a_tar = img_tar[:,:,1]
+    b_tar = img_tar[:,:,2]
+   
+    lmean = np.mean(l)
+    amean = np.mean(a)
+    bmean = np.mean(b)
+
+    tar_lmean = np.mean(l_tar)
+    tar_amean = np.mean(a_tar)
+    tar_bmean = np.mean(b_tar)
+
+    src_l_std = np.std(l)
+    src_a_std = np.std(a)
+    src_b_std = np.std(b)
+
+    tar_l_std = np.std(l_tar)
+    tar_a_std = np.std(a_tar)
+    tar_b_std = np.std(b_tar)
+    
+
+    lstar_src = l - lmean 
+    astar_src = a - amean 
+    bstar_src = b - bmean 
+
+    ldash1 = (tar_l_std/src_l_std) * lstar_src
+    adash1 = (tar_a_std/src_a_std) * astar_src
+    bdash1 = (tar_b_std/src_b_std) * bstar_src
+
+    ldash_new = ldash1 + tar_lmean
+    adash_new = adash1 + tar_amean
+    bdash_new = bdash1 + tar_bmean
+
+    combined_CAM[:,:,0] = ldash_new
+    combined_CAM[:,:,1] = adash_new
+    combined_CAM[:,:,2] = bdash_new
+  
+
+    img_src_final = convert_color_space_CIECAM97s_to_RGB(combined_CAM)
+    final_img = convert_color_space_RGB_to_BGR(img_src_final)
+    
+    return final_img
 
 def color_transfer(img_RGB_source, img_RGB_target, option):
     if option == 'in_RGB':
@@ -179,6 +253,11 @@ def color_transfer(img_RGB_source, img_RGB_target, option):
     elif option == 'in_CIECAM97s':
         img_RGB_new = color_transfer_in_CIECAM97s(img_RGB_source, img_RGB_target)
     return img_RGB_new
+
+
+
+
+
 
 if __name__ == "__main__":
     print('==================================================')
@@ -201,12 +280,13 @@ if __name__ == "__main__":
 
 
 
-    img_BGR = color_transfer(img_RGB_source, img_RGB_target, option='in_Lab')
+#    img_BGR = color_transfer(img_RGB_source, img_RGB_target, option='in_Lab')
+    img_BGR = color_transfer(img_RGB_source, img_RGB_target, option='in_CIECAM97s')
     img_BGR = np.uint8(np.clip(img_BGR,0,255))
     print(np.max(img_BGR))
     print("RMSE:", np.sqrt((img_BGR - rimage)**2).mean())
-    cv2.imwrite('path_file_image_result_in_Lab.png', img_BGR)
     # todo: save image to path_file_image_result_in_Lab
+    cv2.imwrite('path_file_image_result_in_Lab.png', img_BGR)
 
     #img_RGB_new_RGB       = color_transfer(img_RGB_source, img_RGB_target, option='in_RGB')
     # todo: save image to path_file_image_result_in_RGB
